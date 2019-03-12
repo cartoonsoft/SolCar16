39dbf0b44adacd0eb2db3c19f66b0c86dd548a0d
pedro.pires <pedro.pires@cartoonsoft.com.br>
2019-03-12 14:54:09 -0300

    adicionando view model

---
+++ AdmCartorio/Models/ArquivoModeloDocxViewModel.cs
@@ -0,0 +1,46 @@
+﻿using AdmCartorio.Models.Attributes;
+using AdmCartorio.Models.Enumeradores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdmCartorio.Models
+{
+    public class ArquivoModeloDocxViewModel
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Display(Name = "Nome Modelo")]
+        [Required(ErrorMessage = "O campo nome do modelo é obrigatório", AllowEmptyStrings = false)]
+        public string NomeModelo { get; set; }
+
+        [Display(Name = "Tipo de Modelo")]
+        [Required(ErrorMessage = "Selecione algum tipo de modelo")]
+        public NaturezaArquivoModeloDocx NaturezaArquivoModeloDocx { get; set; }
+
+        [Display(Name = "Arquivo")]
+        [RequiredHttpPostedFileBase(ErrorMessage = "Selecione um arquivo.")]
+        [IsWordFile(ErrorMessage = "O arquivo deve ser do tipo '.docx' ")]
+        public List<HttpPostedFileBase> Files { get; set; }
+
+        #region | Dados nao obrigatorios |
+        [Display(Name = "Log Arquivo")]
+        public LogArquivoModeloDocxViewModel LogArquivoModeloDocxViewModel { get; set; }
+
+        [Display(Name = "Caminho Arquivo")]
+        public string CaminhoArquivo { get; set; }
+
+        [Display(Name = "Nome do Arquivo")]
+        public string NomeArquivo { get; set; }
+
+        [Display(Name = "Extensao Arquivo")]
+        public string ExtensaoArquivo { get; set; }
+
+        [Display(Name = "Bytes Arquivo")]
+        public byte[] ArquivoByte { get; set; }
+        #endregion
+    }
+}
---
+++ AdmCartorio/Models/Enumeradores/NaturezaArquivoModeloDocx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace AdmCartorio.Models.Enumeradores
+{
+    [Flags]
+    public enum NaturezaArquivoModeloDocx
+    {
+        [Description("Imóveis")]
+        Imoveis = 1,
+        [Description("Civíl")]
+        Civil = 2
+    }
+}
