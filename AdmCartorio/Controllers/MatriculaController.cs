dd53aae5e7e4cd44720acf2516a18a9b8a89c3d2
pedro.pires <pedro.pires@cartoonsoft.com.br>
2019-03-28 16:55:13 -0300

    usando modelo e copiando string absoluta

--- AdmCartorio/Controllers/MatriculaController.cs
+++ AdmCartorio/Controllers/MatriculaController.cs
@@ -1,9 +1,11 @@
 ﻿using AdmCartorio.Models;
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -48,49 +50,140 @@ namespace AdmCartorio.Controllers
             return View(dados);
         }
 
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(MatriculaAtoViewModel modelo)
+        {
+            string filePath = Server.MapPath($"~/App_Data/Arquivos/TesteModelo.docx");
+
+            try
+            {
+                //Ajusta a string de ato(HTML) -> ato(String)
+                modelo.Ato = ConvertHtmlToString(modelo.Ato);
+
+
+                if (ModelState.IsValid)
+                {
+                    using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+                    {
+
+                        //Pulando linhas por segurança
+                        DocX docX = DocX.Load(fileStream);
+                        docX.InsertParagraph();
+                        docX.InsertParagraph().Append(" meu texto").SpacingAfter(5);
+                        fileStream.Close();
+                        docX.SaveAs(filePath);
+
+                        // Gravar no banco o array de bytes
+                        var arrayBytesNovo = System.IO.File.ReadAllBytes(filePath);
+
+                    }
+                }
+                return View(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// Função que retorna o ato de html para string
+        /// </summary>
+        /// <param name="ato">ATO como HTML</param>
+        /// <returns>ato como string</returns>
+        private static string ConvertHtmlToString(string ato)
+        {
+            var documentoHtml = new HtmlDocument();
+            StringBuilder st = new StringBuilder();
+
+            documentoHtml.LoadHtml(ato);
+
+            foreach (var linhaHtml in documentoHtml.DocumentNode.ChildNodes)
+            {
+                switch (linhaHtml.Name)
+                {
+                    case "p":
+                    case "h1":
+                    case "h2":
+                    case "h3":
+                    case "h4":
+                    case "h5":
+                    case "h6":
+                        {
+                            st.Append(linhaHtml.InnerHtml);
+                            st.AppendLine();
+                            break;
+                        }
+                }
+            }
+
+            return st.ToString();
+        }
+
+        /// <summary>
+        /// Função que monta uma string HTML para mostrar na tela exatamente 
+        /// oque esta escrito no documento
+        /// </summary>
+        /// <returns>string HTML</returns>
         public string UsaModeloParaAto()
         {
-            string textoFormatado = null;
+            StringBuilder textoFormatado = new StringBuilder();
 
             string filePath = Server.MapPath($"~/App_Data/Arquivos/TesteModelo.docx");
             try
             {
                 using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
                 {
+                    //Carrega o Modelo
                     DocX docX = DocX.Load(fileStream);
-                    var texto = docX.Text.ToArray();
-
-                    for (int i = 0; i < texto.Length; i++)
+                    //Varre todos os paragrafos do Modelo
+                    foreach (var paragrafo in docX.Paragraphs)
                     {
-                        if (texto[i] == '[')
+
+                        if (paragrafo.Text != "")
                         {
-                            i++;
-                            string nomeCampo = null;
-                            string resultadoQuery = null;
-                            while (texto[i] != ']')
+                            StringBuilder textoParagrafo = new StringBuilder();
+                            for (int i = 0; i < paragrafo.Text.Length; i++)
                             {
-                                nomeCampo += texto[i].ToString().Trim();
-                                i++;
-                                if (i >= texto.Length || texto[i] == '[')
+                                if (paragrafo.Text[i] == '[')
                                 {
-                                    return "Arquivo com campos corrompidos, verifique o modelo";
+                                    i++;
+                                    string nomeCampo = string.Empty;
+                                    string resultadoQuery = string.Empty;
+                                    while (paragrafo.Text[i] != ']')
+                                    {
+                                        nomeCampo += paragrafo.Text[i].ToString().Trim();
+                                        i++;
+                                        if (i >= paragrafo.Text.Length || paragrafo.Text[i] == '[')
+                                        {
+                                            return "Arquivo com campos corrompidos, verifique o modelo";
+                                        }
+                                    }
+                                    //Buscar dado da pessoa aqui
+                                    resultadoQuery = "teste query";
+
+                                    //atualiza o texto formatado
+                                    textoParagrafo.Append(resultadoQuery);
                                 }
+                                else
+                                {
+                                    //caso não seja um campo somente adiciona o caractere
+                                    textoParagrafo.Append(paragrafo.Text[i].ToString());
+                                }
+
                             }
-                            //Buscar dado da pessoa aqui
-                            resultadoQuery = "teste query";
-
-                            //atualiza o texto formatado
-                            textoFormatado += resultadoQuery;
+                            // Populando campo de retorno
+                            textoFormatado.Append($"<p>{textoParagrafo}</p>");
                         }
-                        else
-                        {
-                            textoFormatado += texto[i].ToString();
-                        }
-
                     }
-                }
-                return textoFormatado;
 
+
+
+                }
+                return textoFormatado.ToString();
             }
             catch (Exception ex)
             {
@@ -98,6 +191,8 @@ namespace AdmCartorio.Controllers
                 return "Ocorreu algum erro ao utilizar o modelo";
             }
         }
+
+
 
 
         public PartialViewResult BuscaMatricula()
--- AdmCartorio/Models/MatriculaAtoViewModel.cs
+++ AdmCartorio/Models/MatriculaAtoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,7 +13,13 @@ namespace AdmCartorio.Models
         public string MatriculaProprietarioAtual { get; set; }
         public string ModeloNome { get; set; }
         public string ModeloTipoAto { get; set; }
+
+
+        //Pode ter mais de um CPF para o mesmo ato
         public string ModeloCPF { get; set; }
+
+        [Required(ErrorMessage ="O ato é obrigatório")]
+        public string Ato { get; set; }
 
         public virtual List<MatriculaViewModel> MatriculasViewModel { get; set; }
         public virtual List<ArquivoModeloSimplificadoViewModel> ModelosSimplificadoViewModel { get; set; }
