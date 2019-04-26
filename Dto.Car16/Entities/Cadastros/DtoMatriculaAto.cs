93ae0f11042fc2781e4d0261b667628af0368390
pedro.pires <pedro.pires@cartoonsoft.com.br>
2019-04-26 10:44:11 -0300

    adicionando funcoes ao lib

---
+++ Dto.Car16/Entities/Cadastros/DtoMatriculaAto.cs
@@ -0,0 +1,28 @@
+﻿using Dto.Car16.Entities.Diversos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto.Car16.Entities.Cadastros
+{
+    public class DtoMatriculaAto
+    {
+        public long MatriculaID { get; set; }
+        public string MatriculaEndereco { get; set; }
+        public string MatriculaOutorgante { get; set; }
+        public string ModeloNome { get; set; }
+        public string ModeloTipoAto { get; set; }
+        public int NumeroPrenotacao { get; set; }
+        public string NomeImovel { get; set; }
+        //Pode ter mais de um CPF para o mesmo ato
+        public string ModeloCPF { get; set; }
+        public string Ato { get; set; }
+        public int NumeroAtoSequencia { get; set; }
+
+        public virtual IEnumerable<DtoMatricula> MatriculasViewModel { get; set; }
+        public virtual IEnumerable<DtoArquivoModeloSimplificadoDocxList> ModelosSimplificadoViewModel { get; set; }
+
+    }
+}
---
+++ LibFunctions/Functions/DataHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibFunctions.Functions
+{
+    public static class DataHelper
+    {
+
+        /// <summary>
+        /// Retorna a data de hoje por extenso
+        /// </summary>
+        /// <returns>DD de MM de YYYY</returns>
+        public static string GetDataPorExtenso()
+        {
+            var date = DateTime.Now.ToLongDateString().Split(',');
+
+            return date[1].Trim();
+        }
+
+    }
+}
