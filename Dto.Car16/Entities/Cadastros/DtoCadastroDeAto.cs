78723dd487b4620c8c52a6dd3779bb4087679460
pedro.pires <pedro.pires@cartoonsoft.com.br>
2019-04-30 16:43:05 -0300

    final do dia

---
+++ Dto.Car16/Entities/Cadastros/DtoCadastroDeAto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto.Car16.Entities.Cadastros
+{
+    public class DtoCadastroDeAto
+    {
+        public DtoPREIMO DtoPREIMO { get; set; }
+        public DtoPESSOA DtoPESSOA { get; set; }
+        public DtoArquivoModeloSimplificadoDocxList DtoArquivoModeloSimplificadoDocxList { get; set; }
+        public long IdTipoAto { get; set; }
+        public string Ato { get; set; }
+        public long? IrParaFicha { get; set; }
+    }
+}
