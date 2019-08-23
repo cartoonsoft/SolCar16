33d7a93a6663c86590cad3529889a1c14863608c
ronaldo.moreira@cartoonsoft.com.br <ronaldo.moreira@cartoonsoft.com.br>
2019-08-23 18:47:59 -0300

    alterações semana

--- Cartorio11RI/ViewModels/PREIMOViewModel.cs
+++ Cartorio11RI/ViewModels/PREIMOViewModel.cs
@@ -8,26 +8,47 @@ namespace Cartorio11RI.ViewModels
 {
     public class PREIMOViewModel
     {
+        [Display(Name = "Num. Prenotação")]
         public long SEQIMO { get; set; }
 
         public long SEQPRE { get; set; }
 
-        public short? SUBD { get; set; }
+        public int? SUBD { get; set; }
+
+        [StringLength(4)]
         public string TIPO { get; set; }
+
+        [StringLength(4)]
         public string TITULO { get; set; }
 
+        [StringLength(100)]
         public string ENDER { get; set; }
+
+        [StringLength(20)]
         public string NUM { get; set; }
+
+        [StringLength(10)]
         public string LOTE { get; set; }
+
+        [StringLength(10)]
         public string QUADRA { get; set; }
 
+        [StringLength(20)]
         public string APTO { get; set; }
+
+        [StringLength(10)]
         public string BLOCO { get; set; }
 
+        [StringLength(50)]
         public string EDIF { get; set; }
+
+        [StringLength(20)]
         public string VAGA { get; set; }
+
+        [StringLength(300)]
         public string OUTROS { get; set; }
-        public int? MATRI { get; set; }
+
+        public int MATRI { get; set; }
 
         public int TRANS { get; set; }
 
@@ -36,6 +57,8 @@ namespace Cartorio11RI.ViewModels
         public int HIPO { get; set; }
 
         public int RD { get; set; }
-        public string CONTRIB { get; set; }
+
+        public long CONTRIB { get; set; }
+
     }
 }
