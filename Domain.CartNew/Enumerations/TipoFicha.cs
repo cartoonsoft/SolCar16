0e02a9d0072d130dcd5af2f1083d9c4bf459f25c
ronaldo.moreira@cartoonsoft.com.br <ronaldo.moreira@cartoonsoft.com.br>
2019-11-11 09:56:17 -0300

    Commit feito antes da instalação do devexpress.

---
+++ Domain.CartNew/Enumerations/TipoFicha.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.CartNew.Enumerations
+{
+    public enum TipoFicha
+    {
+        [Description("Indefinido")]
+        Registro = 0,
+        [Description("Frente")]
+        Averbacao = 1,
+        [Description("Verso")]
+        AtoInicial = 2
+    }
+}
---
+++ WebAppTeste/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppTeste.MvcApplication" Language="C#" %>
